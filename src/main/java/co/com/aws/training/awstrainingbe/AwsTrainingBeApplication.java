c690085f5d2e3e569ca01be1d7958958fffa4913
Jhonattan Fernandez <j.fernandezvargas@globant.com>
2020-07-28 14:28:31 -0500

    initial commit

---
+++ src/main/java/co/com/aws/training/awstrainingbe/AwsTrainingBeApplication.java
@@ -0,0 +1,13 @@
+package co.com.aws.training.awstrainingbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsTrainingBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsTrainingBeApplication.class, args);
+	}
+
+}
